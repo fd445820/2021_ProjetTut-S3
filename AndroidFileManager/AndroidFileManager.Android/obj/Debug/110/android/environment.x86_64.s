ebac1e4ba1db75e8d17236b1f0c91a6205cb23b0
cauvezzito <louiscauvet8@gmail.com>
2021-11-08 15:59:11 +0100

    maj

--- AndroidFileManager/AndroidFileManager.Android/obj/Debug/110/android/environment.x86_64.s
+++ AndroidFileManager/AndroidFileManager.Android/obj/Debug/110/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"91644550-70f7-49cb-bc46-37e4aa22a5ba"
+	.asciz	"f0bade95-9b0d-4cd7-a96f-b8aefad6ae64"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
